9a7d45902da8d2fe89bf3577ac59459b16c815f4
sachinnair92 <sachinnair92@gmail.com>
2016-04-04 10:15:24 +0530

    first commit

---
+++ app/src/main/java/app/voodoo/webpms/Splashscreen.java
@@ -0,0 +1,34 @@
+package app.voodoo.webpms;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by voodoo on 4/4/16.
+ */
+public class Splashscreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash_screen);
+        Thread myThread =new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(10000);
+                    Intent startMainScreen = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(startMainScreen);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+}
